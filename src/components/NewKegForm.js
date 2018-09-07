757f992d727774066187a257154011ce505c5984
protatodev <thaddonaghue@yahoo.com>
2018-09-07 10:39:47 -0700

    further build out newkegform component

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,37 +1,57 @@
 import React from 'react';
-
-const NewKegForm = (props) => {
-  let toggleForm = false;
-
-  const showForm = () => {
-    toggleForm = !toggleForm;
+class NewKegForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggleForm: false
+    }
+    this.showForm = this.showForm.bind(this);
+    this.handleAddKeg = this.handleAddKeg.bind(this);
   }
 
-  const handleAddKeg = (e) => {
+  showForm() {
+    this.setState(prevState => ({ toggleForm: !prevState.toggleForm }));
+  }
+
+  handleAddKeg(e) {
+    e.preventDefault();
+    
     const name = e.target.elements.name.value;
     const brand = e.target.elements.brand.value;
     const price = e.target.elements.price.value;
     const alcohol = e.target.elements.alcohol.value;
+
+    const keg = {
+      name: name,
+      brand: brand,
+      price: price,
+      alcohol: alcohol
+    };
+
+    this.props.handleAddKeg(keg);
   }
 
-  return(
-    <div>
-      <button onClick={showForm}>Add New Keg</button>
-      {
-        toggleForm && 
-        <form onSubmit={handleAddKeg}>
-          <label for="name">Enter New Keg Name:</label>
-          <input type="text" name="name" placeholder="e.g, Blue Moon"/>
-          <label for="brand">Enter Brand Name:</label>
-          <input type="text" name="brand" placeholder="e.g, Deschutes"/>
-          <label for="price">Enter Price:</label>
-          <input type="number" name="price" placeholder="e.g, 5"/>
-          <label for="alcohol">Enter Alcohol Content:</label>
-          <input type="number" name="alcohol" placeholder="e.g, 10.53"/>
-        </form>
-      }
-    </div>
-  );
+  render() {
+    return(
+      <div>
+        <button onClick={this.showForm}>Add New Keg</button>
+        {
+          this.state.toggleForm && 
+          <form onSubmit={this.handleAddKeg}>
+            <label htmlFor="name">Enter New Keg Name:</label>
+            <input type="text" name="name" placeholder="e.g, Blue Moon"/>
+            <label htmlFor="brand">Enter Brand Name:</label>
+            <input type="text" name="brand" placeholder="e.g, Deschutes"/>
+            <label htmlFor="price">Enter Price:</label>
+            <input type="number" name="price" placeholder="e.g, 5"/>
+            <label htmlFor="alcohol">Enter Alcohol Content:</label>
+            <input type="number" name="alcohol" placeholder="e.g, 10.53"/>
+            <button type="submit">Add Keg</button>
+          </form>
+        }
+      </div>
+    );
+  }
 }
 
 export default NewKegForm;
