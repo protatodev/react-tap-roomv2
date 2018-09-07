c3adc87eaadff28f0c79a52a09e8734ab9bd707f
protatodev <thaddonaghue@yahoo.com>
2018-09-07 10:07:49 -0700

    further build out kegsList component

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -6,8 +6,30 @@ class KegList extends React.Component {
   
   constructor(props) {
     super(props);
+    this.handleAddKeg = this.handleAddKeg.bind(this);
+    this.handleRemoveKeg = this.handleRemoveKeg.bind(this);
+    this.handleRemoveAllKegs = this.handleRemoveAllKegs.bind(this);
     this.state = {
       kegs: []
+    }
+  }
+
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('kegs');
+      const kegs = JSON.parse(json);
+
+      if(kegs) this.setState(() => ({ kegs }));
+    }
+    catch(e) {
+      // Do nothing
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.kegs.length !== this.state.kegs.length) {
+      const json = JSON.stringify(this.state.kegs);
+      localStorage.setItem('kegs', json);
     }
   }
 
@@ -26,16 +48,20 @@ class KegList extends React.Component {
   render() {
     return(
       <div>
+        {this.state.kegs.length > 0 && <button onClick={this.handleRemoveAllKegs}>Remove All Kegs</button>}
         {this.state.kegs.length === 0 && <p>You haven't added any kegs yet!</p>}
         {
           this.state.kegs.map(keg => {
             return (
-              <Keg 
-                name={keg.name} 
-                brand={keg.brand}
-                price={keg.price}
-                alcohol={keg.alcohol}  
+              <div>
+                <Keg 
+                  name={keg.name} 
+                  brand={keg.brand}
+                  price={keg.price}
+                  alcohol={keg.alcohol}  
                 />
+                <small><button onClick={this.handleRemoveKeg(keg)}>Remove</button></small>
+              </div>
             );
           })
         }
